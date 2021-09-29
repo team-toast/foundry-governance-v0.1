bf7d1622d8ad3d185499ac068eaa47311199003f
Schalk Dormehl <schalk.dormehl@gmail.com>
2021-09-29 09:36:16 +0200

    fixes to total supply related items

--- contracts/gFry.sol
+++ contracts/gFry.sol
@@ -12,43 +12,45 @@ contract gFRY is Comp
         Comp(address(this))
     {
         governator = msg.sender;
-        totalSupply = 0;
+        burn(balances[msg.sender]); // TODO : update spec doc
+        require(totalSupply == 0, "no tokens should exist"); // TODO : update spec doc
     }
 
-    function mint(address to, uint96 amount) public {
+    function mint(address _to, uint96 _amount) public {
         require(msg.sender == governator, "Comp::_mint: That account cannot mint");
-        require(to != address(0), "Comp::_mint: cannot mint to the zero address");
+        require(_to != address(0), "Comp::_mint: cannot mint to the zero address");
         
-        balances[to] = add96(balances[to], amount, "Comp::_mint: user balance overflows");
-        totalSupply = add96(uint96(totalSupply), amount, "Comp::_mint: totalSupply overflows");
-        emit Transfer(address(0x0), to, amount);
+        balances[_to] = add96(balances[_to], _amount, "Comp::_mint: user balance overflows");
+        totalSupply = add96(uint96(totalSupply), _amount, "Comp::_mint: totalSupply overflows");
+        emit Transfer(address(0x0), _to, _amount);
 
-        _moveDelegates(address(0x0), to, amount);
+        _moveDelegates(address(0x0), _to, _amount);
     }
 
-    function burn(uint96 amount) public {
+    function burn(uint96 _amount) public {
         require(msg.sender != address(0), "Comp::_burn: cannot burn from the zero address");
 
-        balances[msg.sender] = sub96(balances[msg.sender], amount, "Comp::_burn: burn underflows");
-        emit Transfer(msg.sender, address(0x0), amount);
+        balances[msg.sender] = sub96(balances[msg.sender], _amount, "Comp::_burn: burn underflows");
+        totalSupply = sub96(uint96(totalSupply), _amount, "Comp::_mint: totalSupply underflows"); // TODO : update spec doc
+        emit Transfer(msg.sender, address(0x0), _amount);
 
-        _moveDelegates(msg.sender, address(0x0), amount);
+        _moveDelegates(msg.sender, address(0x0), _amount);
     }
 
-    function transferFrom(address src, address dst, uint rawAmount) external returns (bool) {
+    function transferFrom(address _src, address _dst, uint _rawAmount) external returns (bool) {
         address spender = msg.sender;
         // Only alteration from original Comp contract
-        uint96 spenderAllowance = msg.sender == governator ? uint96(-1) : allowances[src][spender];
-        uint96 amount = safe96(rawAmount, "Comp::approve: amount exceeds 96 bits");
+        uint96 spenderAllowance = msg.sender == governator ? uint96(-1) : allowances[_src][spender];
+        uint96 amount = safe96(_rawAmount, "Comp::approve: amount exceeds 96 bits");
 
-        if (spender != src && spenderAllowance != uint96(-1)) {
+        if (spender != _src && spenderAllowance != uint96(-1)) {
             uint96 newAllowance = sub96(spenderAllowance, amount, "Comp::transferFrom: transfer amount exceeds spender allowance");
-            allowances[src][spender] = newAllowance;
+            allowances[_src][spender] = newAllowance;
 
-            emit Approval(src, spender, newAllowance);
+            emit Approval(_src, spender, newAllowance);
         }
 
-        _transferTokens(src, dst, amount);
+        _transferTokens(_src, _dst, amount);
         return true;
     }
 }
